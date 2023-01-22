35f8eca1c757a8eb07044267e5a11c2d13ec9878
mo <mohamad.houmani.eb7@hotmail.com>
2023-01-22 14:15:20 +0200

    Add score and Finalize details

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let ps=0;
+let cs=0;
+
 function GetComputerChoice(){
 let x = Math.floor (Math.random() * 3) ;
 console.log(x);
@@ -13,6 +16,11 @@ function play(x,p)
 {
     let player=p;
     let y=0;
+    let com="";
+    if(x==0){com="rock";
+    }
+    if(x==1){com="paper";}
+    if(x==2){com="scissors";}
     if(player.toUpperCase()=="ROCK")
     {y=0;
     ;}
@@ -25,35 +33,67 @@ function play(x,p)
         
 
     let s = y-x;
-    
+    const div= document.querySelector('div.Results')
     if (s==0)
-    {console.log(`draw`);
-return 0;}
+    {div.textContent="draw";
+    
+    }
     else if (s==1 || s==-2)
-    {console.log(`You win!${player} beats`);
-return 1;}
+    {div.textContent=`You win!${player} beats ${com}`;
+    const spanh=document.querySelector('.hscore')
+    ps=ps+1;
+    spanh.textContent=`${ps}`;
+    }
     else if (s==2 || s==-1)
-    {console.log(`you lose`);
-return -1;}
+    {div.textContent=`you lose!${player} beats ${com}`;
+    const spanc=document.querySelector('.cscore');
+    cs=cs+1;
+    spanc.textContent=`${cs}`;
+    
+}
 
 }
-function play5(){
+// function play5(){
    
-    let hum=0;
-    for (i=5;i>0;i--)
-    {
-        hum+=play(GetComputerChoice(),PlayerSelection());
-    }
-    if (hum<0)
-    {
-        console.log("GAME OVER! YOU LOSE!");
-    }
-    else if(hum>0){
-        console.log("GAME OVER! YOU WIN!");
-    }
-    else if (hum=0)
-    {
-        console.log("GAME OVER! ITS A DRAW!");
-    }
+//     let hum=0;
+//     for (i=5;i>0;i--)
+//     {
+//         hum+=play(GetComputerChoice(),PlayerSelection());
+//     }
+//     if (hum<0)
+//     {
+//         console.log("GAME OVER! YOU LOSE!");
+//     }
+//     else if(hum>0){
+//         console.log("GAME OVER! YOU WIN!");
+//     }
+//     else if (hum=0)
+//     {
+//         console.log("GAME OVER! ITS A DRAW!");
+//     }
     
+// }
+
+{
+const buttons= document.querySelectorAll('button')
+buttons.forEach((button)=>{
+    button.addEventListener('click',function eventss(e){
+        if(ps>4||cs>4)
+        {
+            
+            const final=document.querySelector('.final')
+            if (ps>4){
+
+            final.textContent="you win";
+            } else{
+                final.textContent="you lose";   
+            }
+            return;
+            button.removeEventListener('click',eventss())
+        }
+        play(GetComputerChoice(),button.textContent)
+        
+    });
+
+});
 }
